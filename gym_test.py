8955350d9d2dc6ac4d7beb920248a30ecdaf028a
daichengkai <daichengkai.cuhk@gmail.com>
2025-03-03 21:23:41 +0800

    updated

---
+++ gym_test.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+env = gym.make("CartPole-v1")
+
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+    action = env.action_space.sample()
+    print(action)
+    observation, reward, terminated, truncated, info = env.step(action)
+    print(observation)
+    print(reward)
+    print(terminated)
+    print(truncated)
+    print(info)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
